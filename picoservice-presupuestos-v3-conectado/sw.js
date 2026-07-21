a552378d39f3bd5b7ba25b7d54d34ce9432a3e11
lautyruiz1207 <lautyruiz1207@gmail.com>
2026-07-21 14:47:45 -0300

    Add files via upload

---
+++ picoservice-presupuestos-v3-conectado/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "taller-control-v1";
+const ASSETS = [
+  "./prototipo-taller.html",
+  "./manifest.webmanifest",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
